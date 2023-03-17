21c3d906957e3cacb79736d69daa3447f286ea4c
Thomas Ingeman-Nielsen <thin@dtu.dk>
2023-03-17 14:12:11 +0100

    Added code for receiving and storing messages in database.

--- index.py
+++ index.py
@@ -21,12 +21,17 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, dbc_css], 
 end_date = datetime.date.today()
 start_date = end_date - relativedelta(months=1)
 
+# -------------------------------------------------------------------------
+# Support functions
+
 def get_db_connection():
+    # establish database connection
     conn = sqlite3.connect('alive_info.db')
     conn.row_factory = sqlite3.Row
     return conn
 
 def get_data(start_date, end_date):
+    # obtain messages from database within daterange
     conn = get_db_connection()
     c = conn.cursor()
     c.execute("SELECT id, client, created, name, value FROM messages WHERE DATE(created) BETWEEN ? AND ?", (start_date, end_date))
@@ -37,21 +42,41 @@ def get_data(start_date, end_date):
         data.append({'id': row[0], 'client': row[1], 'name': row[3], 'value': row[4], 'created': row[2]})
     return pd.DataFrame(data)
 
-# @app.server.route('/data')
-# def data():
-#     start_date_str = request.args.get('start_date')
-#     end_date_str = request.args.get('end_date')
-#     start_date = datetime.datetime.fromisoformat(start_date_str).date()
-#     end_date = datetime.datetime.fromisoformat(end_date_str).date()
-#     conn = get_db_connection()
-#     c = conn.cursor()
-#     c.execute("SELECT id, client, created, name, value FROM messages WHERE DATE(created) BETWEEN ? AND ?", (start_date, end_date))
-#     rows = c.fetchall()
-#     conn.close()
-#     data = []
-#     for row in rows:
-#         data.append({'id': row[0], 'client': row[1], 'name': row[3], 'value': row[4], 'created': row[2]})
-#     return jsonify(data)
+# -------------------------------------------------------------------------
+# Functions related to submission of messages
+
+@server.route('/submit_data', methods=['POST'])
+def submit_data():
+    # Store posted messages to database
+    resp = []
+    try:
+        request_data = request.get_json()
+    except:
+        resp.append('cannot get JSON')
+        return '; '.join(resp)
+    if (request_data is not None) & (len(request_data) > 0):
+        conn = get_db_connection()
+        resp.append('db opened')
+        if 'client' in request_data:
+            client = request_data.pop('client', 'unknown')
+            resp.append(client)
+        for k,v in request_data.items():
+            conn.execute('INSERT INTO messages (name, value, client) VALUES (?, ?, ?)', (k, v, client))
+            resp.append('{0}: {1}'.format(k,v))
+        conn.commit()
+        conn.close()
+        resp.append('db closed')
+    return '; '.join(resp)
+
+@server.route('/info', methods=['GET'])
+def serve_info():
+    # provide return message to client
+    # message is currently not used on client
+    # but feature could be implemented to update settings on client.
+    return '2222:127.0.0.1:22'
+
+# -------------------------------------------------------------------------
+# Functions related to serving of message list
 
 @app.callback(Output('table', 'data'),
               Output('name-dropdown', 'options'),
@@ -59,10 +84,8 @@ def get_data(start_date, end_date):
               Input('date-range-picker', 'end_date'),
               Input('name-dropdown', 'value'))
 def update_data(start_date, end_date, value):
+    # Update table data (list of messages)
     df = get_data(start_date, end_date)
-    #url = url_for('data', _external=True, start_date=start_date, end_date=end_date)
-    #response = requests.get(url)
-    #df = pd.read_json(response.content)
     list_of_options = df['name'].sort_values().unique()
     dropdown = [{'label': x,  'value': x} for x in list_of_options ]
 
@@ -75,6 +98,8 @@ def update_data(start_date, end_date, value):
 
     return df.to_dict('records'), dropdown
 
+# -------------------------------------------------------------------------
+# definition of app layout
 
 app.layout = html.Div(children=[
     html.Div([
@@ -90,7 +115,6 @@ app.layout = html.Div(children=[
             start_date=start_date,
             end_date=end_date
         ),
-        #dcc.Interval(id='interval', interval=5000, n_intervals=0),
         dash_table.DataTable(
             id='table',
             columns=[
